64c8d000c93c7068341722e78792ce263096524a
jurgengalicia <jurgen.galicia@gmail.com>
2021-07-21 22:06:16 -0700

    added more fields to shows

--- commands/features/show.js
+++ commands/features/show.js
@@ -16,17 +16,21 @@ module.exports = {
 		const { results } = await fetch(`https://imdb-api.com/en/API/SearchSeries/${IMDB_tok}/${query}`).then(response => response.json());
 		if(!results.length) return msg.channel.send(`No results found for the movie **${args.join(' ')}**`);
 		const { id } = results[0];
-		const { title, stars, image, year, tagline, companies } = await fetch(`https://imdb-api.com/en/API/Title/${IMDB_tok}/${id}`).then(response => response.json());
-		const studio = (companies.split(',')[0]) ? `${companies.split(',')[0]}` : '---';
+		const { title, stars, image, year, tagline, companies, genres } = await fetch(`https://imdb-api.com/en/API/Title/${IMDB_tok}/${id}`).then(response => response.json());
+		const studio = companies ? companies : '---';
+		const starring = stars ? stars : '---';
+		const genre = genres ? genres : '---';
 		const queryInfo = new Discord.MessageEmbed()
 			.setColor('#DE9C38')
 			.setTitle(title)
 			.setURL(`https://www.imdb.com/title/${id}/`)
 			.setFooter('https://www.imdb.com/', 'https://static.wikia.nocookie.net/logopedia/images/8/83/IMDb_Logo.svg/revision/latest/scale-to-width-down/1000?cb=20200925193230');
 		if(tagline) queryInfo.setDescription(tagline);
+
 		if(!image.includes('nopicture')) queryInfo.setImage(image);
 		queryInfo.addFields(
-			{ name: 'Starring', value: stars },
+			{ name: 'Starring', value: starring },
+			{ name: 'Genres', value: genre },
 			{ name: 'Studio', value: studio, inline: true },
 			{ name: 'Release', value: year, inline: true },
 		);
